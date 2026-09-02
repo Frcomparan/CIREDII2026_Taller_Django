87675f4e4bf56c1bf005484df92d2f62404408bf
Frcomparan <frcomparan@gmail.com>
2026-09-02 11:18:24 -0600

    add views, urls and forms

---
+++ comandas/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+
+from .models import Mesero, Producto
+
+
+class SeleccionarMeseroForm(forms.Form):
+    mesero = forms.ModelChoiceField(
+        queryset=Mesero.objects.none(),
+        empty_label=None,
+        label="Selecciona tu nombre",
+        widget=forms.RadioSelect,
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["mesero"].queryset = Mesero.objects.filter(activo=True)
+
+class AgregarProductoForm(forms.Form):
+    producto = forms.ModelChoiceField(
+        queryset=Producto.objects.none(),
+        label="Producto",
+    )
+    cantidad = forms.IntegerField(
+        min_value=1,
+        initial=1,
+        label="Cantidad",
+        widget=forms.NumberInput(attrs={"min": 1}),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["producto"].queryset = (
+            Producto.objects.filter(
+                activo=True,
+                categoria__activa=True,
+            )
+            .select_related("categoria")
+            .order_by("categoria__nombre", "nombre")
+        )
+
+class ActualizarCantidadForm(forms.Form):
+    cantidad = forms.IntegerField(
+        min_value=1,
+        label="Cantidad",
+        widget=forms.NumberInput(attrs={"min": 1}),
+    )
