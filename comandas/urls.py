4b15e3c2883a2c5cea45598ed49677fddc454a9b
Frcomparan <frcomparan@gmail.com>
2026-09-02 12:27:34 -0600

    urls and templates e1

--- comandas/urls.py
+++ comandas/urls.py
@@ -4,4 +4,18 @@ from . import views
 app_name = "comandas"
 
 urlpatterns = [
-  path("", views.tablero_mesas, name="tablero_mesas"),
+    path("", views.tablero_mesas, name="inicio"),
+    path("mesas/", views.tablero_mesas, name="tablero_mesas"),
+    path(
+      "meseros/seleccionar/",
+      views.seleccionar_mesero,
+      name="seleccionar_mesero"
+    ),
+    path(
+      "meseros/cambiar/",
+      views.cambiar_mesero,
+      name="cambiar_mesero"
+    )
+]
+
+
